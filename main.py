e6aedfcfb8ad9d1bcd2eb66dcbec24292fa130e1
JarredTD <jarred.desrosiers@yahoo.com>
2023-02-23 18:16:47 -0500

    init

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from pyfirmata import ArduinoMega, util
+import pyfirmata
+import time
 
-board = ArduinoMega('/dev/tty.usbserial-A6008rIF')
-board.digital[13].write(0)
+# Set up the Arduino board
+board = pyfirmata.Arduino('devtty') # replace 'COM3' with the appropriate port for your board
+board.digital[13].mode = pyfirmata.OUTPUT # set the digital pin 13 (built-in LED) as an output
 
-# it = util.Iterator(board)
-# it.start()
-# board.analog[0].enable_reporting()
-# board.analog[0].read()
+# Blink the LED rapidly
+count = 0
+while True:
+    board.digital[13].write(1) # turn the LED on
+    time.sleep(0.1) # wait for 0.1 seconds
+    board.digital[13].write(0) # turn the LED off
+    time.sleep(0.1) # wait for 0.1 seconds
+    print(f"Iteration: {count}")
+    count+=1
