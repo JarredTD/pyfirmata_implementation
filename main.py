0cdf07ff4c767998dd3b7dbc3613bccddbeead2c
JarredTD <jarred.desrosiers@yahoo.com>
2023-02-26 23:33:31 -0500

    init

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import pyfirmata
 import time
+import Getch
 
 # Set up the Arduino board
-board = pyfirmata.Arduino('devtty') # replace 'COM3' with the appropriate port for your board
+board = pyfirmata.Arduino('/dev/cu.usbmodem101') # replace 'COM3' with the appropriate port for your board
 board.digital[13].mode = pyfirmata.OUTPUT # set the digital pin 13 (built-in LED) as an output
 
-# Blink the LED rapidly
-count = 0
 while True:
-    board.digital[13].write(1) # turn the LED on
-    time.sleep(0.1) # wait for 0.1 seconds
-    board.digital[13].write(0) # turn the LED off
-    time.sleep(0.1) # wait for 0.1 seconds
-    print(f"Iteration: {count}")
-    count+=1
+    output = Getch.get_pulse_width(.1)
+    board.digital[13].write(output) # turn the LED on
+    print(output)
