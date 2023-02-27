2a69ce0e60cc7bd4e2d92dcafe79fac22a72edd6
JarredTD <jarred.desrosiers@yahoo.com>
2023-02-26 23:46:18 -0500

    Updated

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import pyfirmata
-import time
-import Getch
+# import pyfirmata
+# import time
+import util
 
-# Set up the Arduino board
-board = pyfirmata.Arduino('/dev/cu.usbmodem101') # replace 'COM3' with the appropriate port for your board
-board.digital[13].mode = pyfirmata.OUTPUT # set the digital pin 13 (built-in LED) as an output
+# # Set up the Arduino board
+# board = pyfirmata.Arduino('/dev/cu.usbmodem101') # replace 'COM3' with the appropriate port for your board
+# board.digital[13].mode = pyfirmata.OUTPUT # set the digital pin 13 (built-in LED) as an output
 
-while True:
-    output = Getch.get_pulse_width(.1)
-    board.digital[13].write(output) # turn the LED on
-    print(output)
+util.set_terminal_raw()
+byte_str = ''
+while (byte_str != b'\x11'):
+    byte_str = util.get_input_byte()
+    print(byte_str)
+
+
+util.restore_terminal_settings()
